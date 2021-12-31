d618d49287ecd09d2dc820116733b854d9587bb2
AWESOME319 <83673925+AWESOME319@users.noreply.github.com>
2021-12-31 08:13:03 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function login(){
+	player_1_name= document.getElementById("player1").value
+ player_2_name= document.getElementById("player2").value
+ localStorage.setItem("player_1_name",player_1_name)
+ localStorage.setItem("player_2_name",player_2_name)
+ window.location="game_page.html"
+ }
